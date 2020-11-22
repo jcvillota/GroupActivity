5c9d0b734770ce1b27a306b60b64d7fed7b18e31
jcvillota <carlo.villota27@gmail.com>
2020-11-22 22:08:24 +0800

    Files Added

---
+++ Student/app/src/main/java/com/perdo/sqliteinventory/POJO.java
@@ -0,0 +1,44 @@
+package com.perdo.sqliteinventory;
+
+public class POJO
+{
+    private String p_name;
+    private  String p_code;
+    private String p_add;
+
+    private int p_id;
+
+    public String getP_name() {
+        return p_name;
+    }
+
+    public void setP_name(String p_name) {
+        this.p_name = p_name;
+    }
+
+    public String getP_code() {
+        return p_code;
+    }
+
+    public void setP_code(String p_code) {
+        this.p_code = p_code;
+    }
+
+    public String getP_add() {
+        return p_add;
+    }
+
+    public void setP_des(String p_add) {
+        this.p_add = p_add;
+    }
+
+    public int getP_id() {
+        return p_id;
+    }
+
+    public void setP_id(int p_id) {
+        this.p_id = p_id;
+    }
+
+
+}
